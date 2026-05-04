9a7ada62cecae021a1d7fa404391e37a15a43a35
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-05-04 16:20:43 +0530

    configure again

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/nation-wants-to-guess/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react(), tailwindcss()],
 })
