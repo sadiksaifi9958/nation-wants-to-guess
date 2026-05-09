2cb53e5b45f61b020bf0b193d1b7e260d2725bf2
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-05-09 14:00:49 +0530

    add condition to change color of timer as time decrease

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,11 +1,15 @@
-function Score({score, highScore, timer}) {
+function Score({ score, highScore, timer }) {
     return (
         <div className="flex w-full justify-between">
+
             <p className="bg-[#0d1040]  border border-[#2233aa] rounded-full px-4 py-1 text-[#aabbff] text-sm font-bold tracking--widest self-start">Score: {score}</p>
-            <p className="bg-[#0d1040]  border border-[#2233aa] rounded-full px-4 py-1 text-[#aabbff] text-md font-bold tracking--widest self-start">Remaining Time: {timer}</p>
+
+            <p className={`border  rounded-full px-4 py-1  text-md font-bold tracking--widest self-start
+            ${timer <= 7 ? 'bg-[#1a0000] text-[#ff5555] border-[#ff2222] animate-pulse' : timer <= 16 ? 'bg-[#1a1200] text-[#ffbb33] border-[#ff8800]' : 'bg-[#0d1040] border-[#2233aa] text-[#aabbff]'}`}>Remaining Time: {timer}s
+            </p>
             <p className="bg-[#0d1040]  border border-[#2233aa] rounded-full px-4 py-1 text-[#aabbff] text-sm font-bold tracking--widest self-end">High Score: {highScore}</p>
-        </div>
+        </div >
     )
-} 
+}
 
 export default Score
