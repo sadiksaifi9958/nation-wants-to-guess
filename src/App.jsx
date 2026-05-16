d94ce17e9f6cb0c040e067384e10b5e802dec07c
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-05-16 19:54:00 +0530

    separate app logic into a custom hook so the app component only handles rendering that make code more readable and easy to debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,21 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import QuestionCard from './components/QuestionCard'
 import Score from './components/Score'
 import Start from './components/Start'
 import QuizOver from './components/QuizOver'
 import TimeOver from './components/TimeOver'
-
+import useQuiz from './hooks/useQuiz'
 
 function App() {
-  const [gameStarted, setGameStarted] = useState(false)
-  const [question, setQuestion] = useState(null)
-  const [questionCount, setQuestionCount] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [answeredInTime, setAnsweredInTime] = useState(false)
-  const [highScore, setHighScore] = useState(() => {
-    if (localStorage.getItem("highScore")) {
-      return Number(localStorage.getItem("highScore"))
-    } else
-      return 0
-  })
-
-  const totalQuestion = 10
-
-  function onStart() {
-    setGameStarted(true)
-  }
-
-  async function fetchQuestion() {
-    setQuestion(null)
-    setLoading(true)
-    setTimeout(async () => {
-      const response = await fetch('https://nwtg-api.onrender.com/questions/random');
-      const data = await response.json()
-      setQuestion(data)
-      setLoading(false)
-    }, 2000)
-    setAnsweredInTime(false)
-  }
-
-  useEffect(() => {
-    fetchQuestion()
-  }, [])
-
-  const [score, setScore] = useState(0)
-  const [quizover, setQuizover] = useState(false)
-  const [selected, setSelected] = useState(null)
-  const [timer, setTimer] = useState(30)
-  const [timeOver, setTimeOver] = useState(false)
-
-
-  function handleCorrect() {
-    const newScore = score + 1
-    setScore(score + 1)
-    setSelected(null)
-    fetchQuestion()
-    setQuestionCount(questionCount + 1)
-    if (newScore > highScore) {
-      setHighScore(newScore)
-
-    }
-    setTimer(30)
-    setAnsweredInTime(false)
-  }
-
-  useEffect(() => {
-    localStorage.setItem("highScore", highScore)
-  }, [highScore])
-
-  function handleWrong() {
-    setQuizover(true)
-    setTimer(30)
-  }
-
-  useEffect(() => {
-    if (!answeredInTime) {
-      if (gameStarted && !loading && question && !quizover) {
-        const interval = setInterval(() => {
-          setTimer((previousTimer) => {
-            if (previousTimer === 0) {
-              setTimeOver(true)
-              clearInterval(interval)
-              return 0
-            }
-            return previousTimer - 1
-          })
-        }, 1000)
-        return () => clearInterval(interval)
-      }
-    }
-  }, [gameStarted, loading, question, quizover, answeredInTime])
-
-
+  const {
+    score, setScore, selected, setSelected,
+    highScore, totalQuestion, fetchQuestion,
+    setQuizover, setQuestionCount, setTimer,
+    setTimeOver, timer, question, gameStarted,
+    quizover, loading, timeOver, questionCount,
+    handleCorrect, handleWrong, onStart,
+    answeredInTime, setAnsweredInTime
+  } = useQuiz()
 
   return (
     <div className="min-h-screen w-full bg-[#04040c] relative overflow-hidden">
@@ -163,7 +88,8 @@ function App() {
                       answeredInTime={answeredInTime}
                       setAnsweredInTime={setAnsweredInTime}
                     />
-                  </>}
+                  </>
+        }
       </div >
     </div >
   )
---
+++ src/hooks/useQuiz.jsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react'
+
+
+export default function useQuiz() {
+    const [gameStarted, setGameStarted] = useState(false)
+    const [question, setQuestion] = useState(null)
+    const [questionCount, setQuestionCount] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [answeredInTime, setAnsweredInTime] = useState(false)
+    const [highScore, setHighScore] = useState(() => {
+        if (localStorage.getItem("highScore")) {
+            return Number(localStorage.getItem("highScore"))
+        } else
+            return 0
+    })
+
+    const totalQuestion = 10
+
+    function onStart() {
+        setGameStarted(true)
+    }
+
+    async function fetchQuestion() {
+        setQuestion(null)
+        setLoading(true)
+        setTimeout(async () => {
+            const response = await fetch('https://nwtg-api.onrender.com/questions/random');
+            const data = await response.json()
+            setQuestion(data)
+            setLoading(false)
+        }, 2000)
+        setAnsweredInTime(false)
+    }
+
+    useEffect(() => {
+        fetchQuestion()
+    }, [])
+
+    const [score, setScore] = useState(0)
+    const [quizover, setQuizover] = useState(false)
+    const [selected, setSelected] = useState(null)
+    const [timer, setTimer] = useState(30)
+    const [timeOver, setTimeOver] = useState(false)
+
+
+    function handleCorrect() {
+        const newScore = score + 1
+        setScore(score + 1)
+        setSelected(null)
+        fetchQuestion()
+        setQuestionCount(questionCount + 1)
+        if (newScore > highScore) {
+            setHighScore(newScore)
+
+        }
+        setTimer(30)
+        setAnsweredInTime(false)
+    }
+
+    useEffect(() => {
+        localStorage.setItem("highScore", highScore)
+    }, [highScore])
+
+    function handleWrong() {
+        setQuizover(true)
+        setTimer(30)
+    }
+
+    useEffect(() => {
+        if (!answeredInTime) {
+            if (gameStarted && !loading && question && !quizover) {
+                const interval = setInterval(() => {
+                    setTimer((previousTimer) => {
+                        if (previousTimer === 0) {
+                            setTimeOver(true)
+                            clearInterval(interval)
+                            return 0
+                        }
+                        return previousTimer - 1
+                    })
+                }, 1000)
+                return () => clearInterval(interval)
+            }
+        }
+    }, [gameStarted, loading, question, quizover, answeredInTime])
+
+    return {
+    gameStarted,
+    question,
+    questionCount,
+    loading,
+    answeredInTime,
+    highScore,
+    totalQuestion,
+    score,
+    setScore,
+    quizover,
+    setQuizover,
+    selected,
+    setSelected,
+    timer,
+    setTimer,
+    timeOver,
+    setTimeOver,
+    fetchQuestion,
+    handleCorrect,
+    handleWrong,
+    onStart,
+    setAnsweredInTime,
+    setQuestionCount,
+}
+}
