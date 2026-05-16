3936ea48e89f324bdc353d2cf3aa05c1439793c1
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-05-16 22:42:48 +0530

    update base path for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/nation-wants-to-guess/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
